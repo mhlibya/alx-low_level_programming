7b0e536f18adab4d84aebe492e76410a186502e4
mhlibya <mhlibya728@gmail.com>
2023-12-15 18:15:12 -0800

    j

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print a c program using printf function
+ *
+ * Return: always return 0 (success)
+ *
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
