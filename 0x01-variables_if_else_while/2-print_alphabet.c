b885e9e27bfe9c3b0268982a69a0cef31cebeb39
mhlibya <mhlibya728@gmail.com>
2023-12-18 13:54:43 -0800

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: I sometimes suffer from insomnia
+ *
+ * Return: always return o
+ *
+ */
+int main(void)
+{
+	char c = 'a';
+	while ('c' <= 'z')
+	{
+	putchar('c');
+	c++;
+	}
+	putchar('\n');
+	return (0);
+}
