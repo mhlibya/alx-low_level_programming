c1656da258643a4a2bdb8dfa304077c8162f6c3c
mhlibya <mhlibya728@gmail.com>
2023-12-18 14:32:53 -0800

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: alphABET
+ *
+ * Return: always return o
+ *
+ */
+int main(void)
+{
+char a = 'a';
+
+char A = 'A';
+
+while (a <= 'z')
+{
+putchar('a');
+a++;
+}
+while (A <= 'A')
+{
+putchar('A');
+A++;
+}
+putchar('\n');
+return (0);
+}
