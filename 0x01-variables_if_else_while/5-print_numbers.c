51dd786cd52635d373e1e061cf93be50f8dcbfcf
mhlibya <mhlibya728@gmail.com>
2023-12-18 15:06:54 -0800

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: numbers
+ *
+ * Return: always return 0
+ *
+ */
+int main(void)
+{
+char a = '0';
+
+while (a <= '9')
+{
+putchar(a);
+a++;
+}
+putchar('\n');
+return (0);
+}
