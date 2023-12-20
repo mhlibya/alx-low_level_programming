34ec77e99981154f87af143d2c9a81f33c5dc578
mhlibya <mhlibya728@gmail.com>
2023-12-20 11:57:09 -0800

    2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: prints the alphabet a -> z
+ *
+ * Return: always return 0
+ */
+
+void print_alphabet(void);
+{
+int c;
+for(c = 'a'; c <= 'z'; c++)
+	_putchar(c);
+_putchar('\n');
+}
