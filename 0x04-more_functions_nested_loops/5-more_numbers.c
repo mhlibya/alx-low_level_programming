6a20cfd4b8a4ccb9d7b9c837368a7d9637c48949
mhlibya <mhlibya728@gmail.com>
2023-12-21 13:21:37 -0800

    qwe

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints more numbers
+ */
+
+void more_numbers(void)
+{
+int i;
+
+int j;
+
+for (i = 0; i < 10; i++)
+{
+	for (j = 0; j < 15; i++)
+		printf("%i", i);
+	printf("\n");
+}
+}
