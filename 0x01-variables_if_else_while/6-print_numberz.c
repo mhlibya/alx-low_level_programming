dd0f76aa8a567c062651a78b8d45d7a470fb5885
mhlibya <mhlibya728@gmail.com>
2023-12-21 02:13:29 -0800

    hyt

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: numberz
+ *
+ * Return: always return 0
+ *
+ */
+int main(void)
+{
+for (int a=0,a<10,a++)
+{
+putchar('a');
+}
+return (0);
+}
