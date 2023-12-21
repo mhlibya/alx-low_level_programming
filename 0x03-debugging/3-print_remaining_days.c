0b70026689737bc782bec961f22c7df07cd16d74
mhlibya <mhlibya728@gmail.com>
2023-12-21 07:31:49 -0800

    sde

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,11 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 400 == 0 && year % 100 == 0) || !(year % 4 == 0))
+	/**
+	* leap year when it
+	* can be devided by 4
+	*/
+	if ((year % 400 == 0 && year % 100 == 0) || (year % 4 == 0))
 	{
 		if (month >= 2 && day >= 60)
 		{
