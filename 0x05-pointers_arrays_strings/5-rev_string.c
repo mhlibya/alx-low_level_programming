3b5e78811153f18743b8e12129dd14bacd5d3f37
mhlibya <mhlibya728@gmail.com>
2024-01-02 13:35:26 -0800

    cjkdb

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - lsfjvnsljfvn
+ *
+ * @s: jgfhd
+ */
+
+void rev_string(char *s)
+{
+int l, i;
+char g;
+
+for (l = 0; s[l] != '\0'; l++)
+	;
+
+for (i = 0; i < 1 / 2; i++)
+{
+g = s[i];
+s[i] = s[l - 1 - i];
+s[l - 1 - i] = g;
+}
+}
