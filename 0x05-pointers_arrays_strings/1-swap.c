bde7533c7ff19d52e6a9666bc49651dca8c4368c
mhlibya <mhlibya728@gmail.com>
2024-01-02 07:44:24 -0800

    gry

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - vrdfiv g ncelscj
+ *
+ * @a : variable
+ * @b : variable
+ * @g : variable
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+int g;
+
+g = *a;
+*a = *b;
+*b = g;
+}
