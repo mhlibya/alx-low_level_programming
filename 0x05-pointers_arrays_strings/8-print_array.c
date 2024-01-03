9573ea555cfb3cb7fd9b6c90691f3544e26c7623
mhlibya <mhlibya728@gmail.com>
2024-01-03 08:02:57 -0800

    gte

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_array - fiv;jsskfm
+ *
+ * @a: slkrfnm;wsm
+ *
+ * @n: pisrjfwprijf
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%i, ", a[i]);
+}
+printf("\n");
+}
