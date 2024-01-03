95ed07d2f65cd97058a8c9786fe32e32276e0060
mhlibya <mhlibya728@gmail.com>
2024-01-03 09:40:54 -0800

    gte

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,6 +13,10 @@ void print_array(int *a, int n)
 int i;
 for (i = 0; i < n; i++)
 {
-printf("%i, ", a[i]);
+if (i != (n - 1))
+	printf("%i, ", a[i]);
+else
+	printf("%i", a[i]);
 }
+printf("\n");
 }
