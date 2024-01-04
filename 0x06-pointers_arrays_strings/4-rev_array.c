a92b917b41535758162912911740d0a1abc061fe
mhlibya <mhlibya728@gmail.com>
2024-01-04 09:09:58 -0800

    fog

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - worifyh
+ *
+ * @a: spigj
+ * @n: wo[ej
+ */
+void reverse_array(int *a, int n)
+{
+int i, j;
+
+j = n;
+for (i = 0; i < n; i++)
+{
+a[i] = a[j];
+j--;
+}
+}
