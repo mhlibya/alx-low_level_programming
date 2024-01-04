4954e9c7db7ce99840b43d90902ca399764cbf26
mhlibya <mhlibya728@gmail.com>
2024-01-04 07:29:32 -0800

    hg

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - sirlygf
+ *
+ * @dest: kshf
+ *
+ * @src: werif
+ *
+ * Return: jfyjkut
+ */
+char *_strcat(char *dest, char *src)
+{
+int c, c2;
+
+c = 0;
+while (dest[c])
+	c++;
+for (c2 = 0; src[c2]; c2++)
+	dest[c++] = src[c2];
+return (dest);
+}
