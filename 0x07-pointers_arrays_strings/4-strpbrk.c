c5fdf6f39699f5231337f469ac706698836644cf
mhlibya <mhlibya728@gmail.com>
2024-01-08 18:15:05 -0800

    joi

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strpbrk - srufh
+ *
+ * @s: pvf
+ * @accept: ldihrg
+ *
+ * Return: rijfg
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int i, j, k;
+char *a;
+
+for (i = 0; s[i] != accept[i]; i++)
+	return (NULL);
+for (j = i, k = 0; a = a, s[j] != '\0'; j++, k++)
+	a[k] = s[j];
+return (a);
+}
