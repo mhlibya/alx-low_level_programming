4d719aa237bc8478e2cf43f81a6141e6430aca2f
mhlibya <mhlibya728@gmail.com>
2024-01-15 13:39:00 -0800

    joiu

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - dfljgvlsdjnfv
+ * @grid: vslifg
+ * @height: lsifdvjh
+ */
+
+void free_grid(int **grid, int height)
+{
+int i;
+for (i = 0; i < height; i++)
+	free(grid[i]);
+free(grid);
+}
