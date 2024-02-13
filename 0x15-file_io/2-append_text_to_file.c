8f9e2e1e463546f792b7d1901ddab840c03b1de0
mhlibya <mhlibya728@gmail.com>
2024-02-13 05:23:47 -0800

    joi

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * append_text_to_file - ksuhf
+ * @filename: wouef
+ * @text_content: ksuhrf
+ * Return: kwurfh
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+int i = 0, file;
+
+if (filename == NULL)
+	return (-1);
+
+if (text_content == NULL)
+	text_content = "";
+
+
+while (text_content[i] != '\0')
+{
+	i++;
+}
+
+file = open(filename, O_WRONLY | O_APPEND);
+
+if (file == -1)
+	return (-1);
+
+write(file, text_content, i);
+
+return (1);
+}
