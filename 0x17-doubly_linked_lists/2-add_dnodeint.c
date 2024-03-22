ef17d34e91c7a5ca8c060a37d0e5f64674b32fba
mhlibya <mhlibya728@gmail.com>
2024-03-21 20:11:22 -0700

    Gg

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,12 +8,28 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-dlistint_t *new = malloc(sizeof(dlistint_t));
+dlistint_t *new, *h;
 
-if (!new)
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
 	return (NULL);
+
 new->n = n;
-new->next = *head;
+new->prev = NULL;
+h = *head;
+
+if (h != NULL)
+{
+	while (h->prev != NULL)
+		h = h->prev;
+}
+
+new->next = h;
+
+if (h != NULL)
+	h->prev = new;
+
 *head = new;
-return (*head);
+
+return (new);
 }
