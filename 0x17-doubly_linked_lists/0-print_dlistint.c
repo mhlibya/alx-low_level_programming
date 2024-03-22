adb13ccd62259abc6bdb0ace902d8c4ef13dc9a6
mhlibya <mhlibya728@gmail.com>
2024-03-21 18:38:27 -0700

    Gg

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include"lists.h"
+
+/**
+ * print_dlistint_t - sdljf
+ * @h: dpfijg
+ * Return: lsirjg
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+size_t i;
+
+for(i = 0; h; i++)
+{
+printf("%d\n", h->n);
+h = h->next;
+}
+return (i);
+}
